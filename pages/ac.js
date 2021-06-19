99cfb2dc61a1e373006e59c5701b81c040a5fdcd
tarita <pepper.ppeatns@gmail.com>
2021-06-19 12:03:00 +0900

    updated AC pages

--- pages/ac.js
+++ pages/ac.js
@@ -18,12 +18,12 @@ const AC=()=>{
         <div name="AC" >
             <div className={layouts.container} >
                 {MsgState &&
-                <div className={styles.msgwrapper}  >
-                    <i className={`fas fa-times ${styles.close}`} onClick={()=>setMsgState(!MsgState)}></i>
-                    <h1 className={styles.msg}>Coming soon...</h1>
-                </div>
-                }          
-                <input className={styles.cersius} type="number" min="18" max="30"  placeholder="88" onFocus={()=>setFocused(true)} onBlur={()=>setFocused(false)}></input>                
+                    <div className={styles.msgwrapper}  >
+                        <i className={`fas fa-times ${styles.close}`} onClick={()=>setMsgState(!MsgState)}></i>
+                        <h1 className={styles.msg}>Coming soon...</h1>
+                    </div>
+                }
+                <input className={styles.cersius} type="number" min="18" max="30" placeholder="88" onFocus={()=>setFocused(true)} onBlur={()=>setFocused(false)} ></input>                
                 <div className={styles.control}>
                     <div className={styles.menu}>
                         <Select options={options}
@@ -32,7 +32,7 @@ const AC=()=>{
                             value={options.find(obj => obj.value === selectedValue)}
                             onChange={()=>setSelectedValue(this)}
                             isSearchable={false}
-                        />     
+                        />
                     </div>
                     <button className={`${layouts.btn} ${layouts.icons}`} ><i className="fas fa-power-off" onClick={()=>null}></i></button>
                     <button className={`${styles.icons} ${layouts.btn}`} ><i className="fas fa-stop"  onClick={()=>null}></i></button>
