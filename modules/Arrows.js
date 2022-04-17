7684d6006751a8841a0f92a04d2cbe9909fe698b
tarita <pepper.ppeatns@gmail.com>
2022-04-17 16:12:53 +0100

    Shorten Arrows.js codes

--- modules/Arrows.js
+++ modules/Arrows.js
@@ -1,23 +1,20 @@
 import layouts from "../styles/Layouts/layouts.module.css"
 
 const Arrows=(props)=>{
+  const ArrowsBtn=(d)=>(
+    <button className={layouts[d]} >
+      <i className={`fas fa-chevron-${d} ${props.arrow} ${layouts.arrbtn}`} onClick={props[d]} ></i>
+    </button>
+  );
     return(
         <div className={layouts.arrows}>
-          <button className={layouts.up} >
-            <i className={`fas fa-chevron-up ${props.arrow} ${layouts.arrbtn}`} onClick={props.up} ></i>
-          </button>
-          <button className={layouts.left} >
-            <i className={`fas fa-chevron-left ${props.arrow} ${layouts.arrbtn} `} onClick={props.left} ></i>
-          </button>
+          {ArrowsBtn('up')}
+          {ArrowsBtn('left')}
           <button className={layouts.enter} >
             <i className={`fas fa-dot-circle  ${props.arrow} ${layouts.entbtn} `} onClick={props.enter} ></i>
           </button>
-          <button className={layouts.right} >
-            <i className={`fas fa-chevron-right  ${props.arrow} ${layouts.arrbtn}`} onClick={props.right} ></i>
-          </button>
-          <button className={layouts.down} >
-            <i className={`fas fa-chevron-down  ${props.arrow} ${layouts.arrbtn}`} onClick={props.down} ></i>
-          </button>
+          {ArrowsBtn('right')}
+          {ArrowsBtn('down')}
         </div>
     )}
 export default Arrows;
